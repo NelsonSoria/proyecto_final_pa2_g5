37b59248c6488bedb390279f1b8792467445c590
Nelson Soria <nelson-soria@hotmail.com>
2023-08-22 16:15:15 -0500

    main

---
+++ src/main/java/com/example/demo/ProyectoFinalPa2G5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalPa2G5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalPa2G5Application.class, args);
+	}
+
+}
